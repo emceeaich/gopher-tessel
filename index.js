857ad85172636e27fe024d48922333db5c924f82
Emma Humphries <ech@emmah.net>
2016-09-15 20:13:50 -0700

    Add blinking lights as a service

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ var climate =    climatelib.use(tessel.port['A']);
 var address =    '192.168.1.101';
 var port    =    7000;
 
+
+// ready lights
+var len = tessel.led.length;
+for (var i = 0; i < len; i++) {
+    tessel.led[i].off();
+}
+
 climate.on('ready', function() {
     console.log('Connected to climate module.');
     serve();
@@ -25,9 +32,14 @@ function serve() {
         console.log('data', data);
         if (data === '\r\n') {
             console.log('Serving index.');
-            socket.write('0About This Service' + '\t' + 'About' + '\t' + address + '\t' + port + '\r\n');
+            socket.write('Welcome to Tessel Gopher\r\n');
+            socket.write('\r\n');
+            socket.write('Powered by node.js\r\n');
+            socket.write('\r\n');
+            socket.write('1About This Service' + '\t' + 'About' + '\t' + address + '\t' + port + '\r\n');
             socket.write('0Local Temperature' + '\t' + 'Temp' + '\t' + address + '\t' + port + '\r\n');
             socket.write('0Local Humidity' + '\t' + 'Humidity' + '\t' + address + '\t' + port + '\r\n');
+            socket.write('0Blinken Lights' + '\t' + 'Blink' + '\t' + address + '\t' + port + '\r\n');
             socket.write('.\r\n');
             socket.end();
         }
@@ -41,7 +53,7 @@ function serve() {
             console.log('Reading climate module temp data');
             climate.readTemperature('f', function(err, temp) {
                 console.log('Serving climate module temp data');
-                socket.write('The current temperature at this device\'s location is ' + temp.toFixed(4) + 'F');
+                socket.write('The current temperature at this device\'s location is ' + temp.toFixed(4) + 'F\r\n');
                 socket.end();
             });
         }
@@ -49,9 +61,19 @@ function serve() {
             console.log('Reading climate module humidity data');
             climate.readHumidity(function (err, humid) {
                 console.log('Serving climate module humidity data');
-                socket.write('The current humidity at this device\'s location is ' + humid.toFixed(4) + '%RH');
+                socket.write('The current humidity at this device\'s location is ' + humid.toFixed(4) + '%RH\r\n');
                 socket.end();
             });
+        }
+        else if (data === 'Blink\r\n') {
+            console.log('Tenth Doctor Joke Here');
+            for(var i = 0; i < 100; i++) {
+                for(var j = 0; j < len; j++) {
+                    tessel.led[j].on().off();
+                }            
+            }
+            socket.write('Tell the Doctor that the Angels have the Tessel.\r\n');
+            socket.end();
         }
         else {
             console.log('Unknown: ' + data);
